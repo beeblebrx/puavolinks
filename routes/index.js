5b68f954e52ef6b9ab2d33766ee6be890b8bba2a
Beeblebrx <nobody@nowhere.no>
2013-03-10 22:45:00 +0200

    Get / prints a list of URLs in DB.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    var pg = require('pg')
+    var psqlClient = new pg.Client(process.env.DATABASE_URL);
+    psqlClient.connect();
+
+    psqlClient.query("SELECT url FROM links", function(err, result) {
+	debugger;
+	if (!err) {
+	    var links = new Array();
+	    for (var index in result.rows) {
+		links.push(result.rows[index].url);
+	    }
+	    res.render('index', { title: 'puavolinks', 'links': links });
+	} else {
+	    res.render('error', { title: 'FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU'} );
+	}
+
+	psqlClient.end();
+    });
 };
