7fccc5d74f44ec63e9776fde1b2a50b6911c9b8c
Beeblebrx <nobody@nowhere.no>
2013-03-26 23:41:14 +0200

    Instead of add 2 or 3 hours to date_posted myself, do conversion in query.

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@
 
 var moment = require('moment');
 
-function range1(i){return i?range1(i-1).concat(i):[]}
+function range1(i){return i?range1(i-1).concat(i):[];}
 
 exports.index = function(req, res){
-    var pg = require('pg')
+    var pg = require('pg');
     var psqlClient = new pg.Client(process.env.DATABASE_URL);
     psqlClient.connect();
 
@@ -25,25 +25,23 @@ exports.index = function(req, res){
 	}
 
 	var numPages = Math.ceil(countResult.rows[0].count / pageSize);
-	debugger;
-	psqlClient.query("SELECT l.url, p.nick, l.date_posted FROM links l, posters p WHERE l.poster = p.pid ORDER BY l.date_posted DESC LIMIT $1 OFFSET $2", [pageSize, page * pageSize], function(err, result) {
+	psqlClient.query("SELECT l.url, p.nick, l.date_posted AT TIME ZONE 'EET' AS date_posted FROM links l, posters p WHERE l.poster = p.pid ORDER BY l.date_posted DESC LIMIT $1 OFFSET $2", [pageSize, page * pageSize], function(err, result) {
 	    if (!err) {
 		var links = [];
 
 		for (var index in result.rows) {
-		    var offset = moment().isDST() ? 3 : 2;
 		    var row = result.rows[index];
 		    links.push({ "url": row.url,
 				 "poster": row.nick,
-				 "date": moment(row.date_posted).add('h', offset).format("DD.MM.YYYY HH:mm")
+				 "date": moment(row.date_posted).format("DD.MM.YYYY HH:mm")
 			       });
 		}
-		
+
 		res.render('index', { title: 'puavolinks', 'links': links, 'pages': range1(numPages), 'pageNum': page + 1 });
 	    } else {
 		res.render('error', { title: 'FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU'} );
 	    }
-	    
+
 	    psqlClient.end();
 	});
     });
